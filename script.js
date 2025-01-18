5d03c44a2a8973c906edb55217574b7965f5a606
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-18 11:08:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //your code here!
-
+let orderedList = document.getElementById("infi-list");
+for(let i = 1; i <= 10; i++){
+    let list = document.createElement("li");
+    list.innerText = "Item" + i;
+    orderedList.appendChild(list);
+}
